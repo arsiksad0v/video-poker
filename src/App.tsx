47be2453d0413c193c2e968deef3960aaf90e2f1
Arstan <arsiksad0v.wgod@gmail.com>
2024-12-24 03:00:21 +0600

    Connected a button to App.tsx, improved styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,44 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React, { useState } from 'react';
+import DeckOfCards from './components/DeckOfCards';
+import PlayingCard from './components/PlayingCardClass';
+import './cards.css';
 
-function App() {
-  const [count, setCount] = useState(0)
+const App: React.FC = () => {
+  const [currentHand, setCurrentHand] = useState<PlayingCard[]>([]);
+
+  const dealCards = () => {
+    const deck = new DeckOfCards();
+    const hand = deck.drawMultipleCards(5); 
+    setCurrentHand(hand);
+  };
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+    <div>
+      {currentHand.length === 0 ? (
+        <button onClick={dealCards}>Раздать карты</button>
+      ) : (
+        <div>
+          <button onClick={dealCards}>Раздать карты</button>
+          <div className="playingCards faceImages">
+            {currentHand.map((card, index) => (
+              <span
+                key={index}
+                className={`card rank-${card.value.toLowerCase()} ${card.symbol}`}
+              >
+                <span className="rank">{card.value}</span>
+                <span className="suit">
+                  {card.symbol === 'diams' && '♦'}
+                  {card.symbol === 'hearts' && '♥'}
+                  {card.symbol === 'clubs' && '♣'}
+                  {card.symbol === 'spades' && '♠'}
+                </span>
+              </span>
+            ))}
+          </div>
+        </div>
+      )}
+    </div>
+  );
+};
 
-export default App
+export default App;
